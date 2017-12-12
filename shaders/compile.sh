03e6709935ba314fe8a0a7732ba96bf511a94b53
zealot <z10ba89@gmail.com>
2017-12-12 18:32:38 +0300

    set relative paths

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,2 +1,2 @@
-/home/zealot/VulkanSDK/1.0.61.1/x86_64/bin/glslangValidator -V shader.vert
-/home/zealot/VulkanSDK/1.0.61.1/x86_64/bin/glslangValidator -V shader.frag
+$VULKAN_HOME/bin/glslangValidator -V shader.vert
+$VULKAN_HOME/bin/glslangValidator -V shader.frag
